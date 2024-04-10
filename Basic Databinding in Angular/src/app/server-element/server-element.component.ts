72e7ad7eb0bf4fe318660eb90641331cef10b44e
DBappa <bappadityadutta2024@gmail.com>
2024-04-10 23:23:56 +0530

    Component Segregation

---
+++ Basic Databinding in Angular/src/app/server-element/server-element.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrl: './server-element.component.css'
+})
+export class ServerElementComponent {
+
+  element:{type:string,name:string,content:string};
+
+}
